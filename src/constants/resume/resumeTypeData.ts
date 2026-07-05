d4786776529a75a13ec04a9f199f8a5b58d0ba63
zura <zuramanagadze1@yahoo.com>
2026-07-05 16:49:06 +0400

    feat: changed resume template previews and resume tpye data configuration data constant

--- src/constants/resume/resumeTypeData.ts
+++ src/constants/resume/resumeTypeData.ts
@@ -9,26 +9,26 @@ interface ResumeTypeDataProps {
 
 export const resumeTypeData: ResumeTypeDataProps[] = [
   {
-    src: "/resume/resume-modern.png",
+    src: "/resume/resume-modern.PNG",
     alt: "resume modern",
     type: "modern",
     delay: 0,
   },
   {
-    src: "/resume/resume-classic.png",
+    src: "/resume/resume-classic.PNG",
     alt: "resume classic",
     type: "classic",
     delay: 0.2,
   },
   {
-    src: "/resume/resume-creative.png",
+    src: "/resume/resume-creative.PNG",
     alt: "resume creative",
     type: "creative",
     delay: 0.4,
   },
 
   {
-    src: "/resume/resume-executive.png",
+    src: "/resume/resume-executive.PNG",
     alt: "resume executive",
     type: "executive",
     delay: 0.6,
