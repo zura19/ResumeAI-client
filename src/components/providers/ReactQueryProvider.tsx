3928ff98e3d4389a90c0d55c7d684e737ae3d5da
zura <zuramanagadze1@yahoo.com>
2026-07-05 21:30:13 +0400

    refactor: stabilize react query client instance

--- src/components/providers/ReactQueryProvider.tsx
+++ src/components/providers/ReactQueryProvider.tsx
@@ -1,13 +1,13 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-interface props {
+const queryClient = new QueryClient();
+
+interface Props {
   children: React.ReactNode;
 }
 
-export default function ReactQueryProvider({ children }: props) {
-  const queryClient = new QueryClient();
-
+export default function ReactQueryProvider({ children }: Props) {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
